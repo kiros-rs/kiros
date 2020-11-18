5f4bd04e951a2497682152ef853533125c109d56
Angus Finch <developer.finchie@gmail.com>
2020-11-18 20:09:21 +0800

    Refactor connection API

--- connection/src/lib.rs
+++ connection/src/lib.rs
@@ -1,32 +1,55 @@
-pub mod usb;
-
 /// A generic API that exposes basic functionality from the method of
 /// connection to enable abstraction over the connection method
 pub trait ConnectionHandler {
+    /// Write a packet over the connection
     fn write(&mut self, packet: Vec<u8>) -> Result<usize, std::io::Error>;
-    fn read(&mut self, buf: &mut Vec<u8>) -> Result<usize, std::io::Error>;
-    // fn open(&mut self, port: &str, baudrate: usize) -> Result<Box<dyn ConnectionHandler>, String>; // This needs to have an abstract way of passing location (port, pins etc)
-    fn get_connection_type(&self) -> ConnectionType;
-    // flush?
+    /// Read a packet over the connection
+    fn read(&mut self) -> Result<usize, std::io::Error>;
+    /// Connect & perform any initialisation required
+    fn connect(&mut self) -> Option<std::io::Error>;
 }
 
-/// All different methods of connection
+/// Additional functionality available when accessing buffered connections
+pub trait BufferedConnectionHandler {
+    /// Flush the connection buffer
+    fn flush(&mut self);
+    /// Read a buffered packet over the connection
+    fn read(&mut self, buf: &mut Vec<u8>) -> Result<usize, std::io::Error>;
+}
+
+/// An API to get basic connection info
+pub trait ConnectionInfo {
+    fn get_connection_type(&self) -> ConnectionType;
+    // connection configuration?
+}
+
+/// All the different methods of connection between nodes
 pub enum ConnectionType {
-    USB,
-    Analogue,
-    Digital,
-    Network(NetworkConnectionType),
+    Wired(WiredConnectionType),
+    Wireless(WirelessConnectionType),
     Virtual(VirtualConnectionType),
 }
 
-/// All different methods of connection over a network
-pub enum NetworkConnectionType {
-    TCP,
-    Bluetooth,
-    // Should expand this with mesh network systems
+/// All the wired methods of connection between nodes
+pub enum WiredConnectionType {
+    USB,
+    Ethernet,
+    Pin(PinConnectionType),
 }
 
-/// All different methods of virtual connection
+/// All the pin-based methods of connection between nodes
+pub enum PinConnectionType {
+    Analogue,
+    Digital,
+}
+
+/// All the wireless methods of connection between nodes
+pub enum WirelessConnectionType {
+    TCP,
+    Bluetooth,
+}
+
+/// All the virtual methods of connection
 pub enum VirtualConnectionType {
     Dataset(String),
     Random { min: usize, max: usize },
